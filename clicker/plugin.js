b0992d4aff784351ab5262e4254b82709be3702b
m-i-z-u-k-i <m-i-z-u-k-i@users.noreply.github.com>
2025-12-26 09:53:24 +0000

    deploy: 76a7cd80c6007035ce2f9033b5187af6c0a5989a

--- clicker/plugin.js
+++ clicker/plugin.js
@@ -1,11 +1,53 @@
 (function(exports) {
 
-"use strict";
+//#region rolldown:runtime
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __commonJS = (cb, mod) => function() {
+	return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+var __copyProps = (to, from, except, desc) => {
+	if (from && typeof from === "object" || typeof from === "function") for (var keys = __getOwnPropNames(from), i = 0, n = keys.length, key; i < n; i++) {
+		key = keys[i];
+		if (!__hasOwnProp.call(to, key) && key !== except) __defProp(to, key, {
+			get: ((k) => from[k]).bind(null, key),
+			enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable
+		});
+	}
+	return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", {
+	value: mod,
+	enumerable: true
+}) : target, mod));
 
+//#endregion
+
+//#region solid-js/web
+var require_web = __commonJS({ "solid-js/web"(exports, module) {
+	module.exports = shelter.solidWeb;
+} });
+
+//#endregion
 //#region plugins/clicker/index.tsx
-const { GuildMemberStore, ChannelStore, SelectedChannelStore, RelationshipStore } = shelter.flux.storesFlat;
-const { util: { getFiber, reactFiberWalker }, observeDom, util: { log } } = shelter;
+var import_web = __toESM(require_web(), 1);
+const { GuildMemberStore, ChannelStore, SelectedChannelStore, RelationshipStore, SoundStore } = shelter.flux.storesFlat;
+const { util: { getFiber, reactFiberWalker }, observeDom, util: { log }, plugin: { store } } = shelter;
+const { TextArea } = shelter.ui;
 const { subscribe } = shelter.plugin.scoped.flux;
+const settings = () => (0, import_web.createComponent)(TextArea, {
+	get value() {
+		return "// please separate allowed user ids with line breaks\n" + (store.ids.join("\n") || "");
+	},
+	mono: true,
+	onInput,
+	"resize-x": true,
+	"resize-y": true
+});
 let lastPlayTime = 0;
 const processedMessageIds = new Set();
 function onLoad() {
@@ -22,6 +64,10 @@ function handleDispatch(payload) {
 	if (!message) return;
 	const content = message.content || "";
 	if (!content.includes("!click")) return;
+	if (!store.ids.includes(message.author.id)) {
+		log("user not in allowed list!");
+		return;
+	}
 	if (processedMessageIds.has(message.id)) {
 		log("duplicate message ignored :3");
 		return;
@@ -40,9 +86,14 @@ function handleDispatch(payload) {
 	log("found valid !click message! playing sound uwu");
 	new Audio("https://cdn.discordapp.com/soundboard-sounds/1442006037522546688").play();
 }
+function onInput(text) {
+	store.ids = text.split("\n").filter((line) => !line.startsWith("//"));
+	log(store.ids);
+}
 
 //#endregion
 exports.onLoad = onLoad
 exports.onUnload = onUnload
+exports.settings = settings
 return exports;
 })({});
